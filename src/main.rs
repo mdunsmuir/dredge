488d24246a7b5cc0578fde36b6ec245ebe84931a
Michael Dunsmuir <mjdunsmuir@gmail.com>
2016-08-26 17:14:56 -0700

    work on ui, shows list w/ selection + scrolling works but not window following scrolling

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,11 @@ enum FSTree {
     Bad
 }
 
-struct UIState {
+struct UI<'a> {
+    rustbox: &'a rustbox::RustBox,
     stack: Vec<std::ffi::OsString>,
-    selected: usize,
+    listing: Vec<(std::ffi::OsString, u64)>,
+    selected: Option<usize>,
     window_top: usize,
 }
 
@@ -122,14 +124,78 @@ impl FSTree {
     }
 }
 
-impl UIState {
+impl<'a> UI<'a> {
 
-    fn new() -> Self {
-        UIState {
+    fn new(rustbox: &'a rustbox::RustBox, fsts: &Contents) -> Self {
+        let mut ui = UI {
+            rustbox: rustbox,
             stack: Vec::new(),
-            selected: 0,
+            listing: Vec::new(),
+            selected: None,
             window_top: 0,
+        };
+
+        ui.load(fsts);
+        ui
+    }
+
+    fn load(&mut self, mut fsts: &Contents) {
+        self.listing.clear();
+
+        for name in self.stack.iter() {
+            fsts = match *fsts.get(name).unwrap() {
+                FSTree::Dir { ref contents, .. } => contents,
+                _ => panic!("expected Dir"),
+            };
         }
+
+        if fsts.is_empty() {
+            self.selected = None;
+        } else {
+            self.selected = Some(0);
+
+            for (name, fst) in fsts {
+                self.listing.push((name.clone(), fst.size()));
+            }
+        }
+
+        self.listing.sort_by_key(|&(_, size)| size );
+    }
+
+    fn draw(&self) {
+        self.rustbox.clear();
+
+        match self.selected {
+            None => self.rustbox.print(
+                0, 0, rustbox::Style::empty(),
+                rustbox::Color::White,
+                rustbox::Color::Default,
+                "<no files>"
+            ),
+
+            Some(i_selected) => {
+                let height = self.rustbox.height();
+                let last_index = // actually last index + 1
+                    std::cmp::min((self.window_top + height), self.listing.len());
+                let to_display = 
+                    &self.listing[self.window_top..last_index];
+
+                for (i, &(ref name, size)) in to_display.iter().enumerate() {
+                    let (front, back) = if i + self.window_top  == i_selected {
+                        (rustbox::Color::Black, rustbox::Color::White)
+                    } else {
+                        (rustbox::Color::White, rustbox::Color::Default)
+                    };
+
+                    self.rustbox.print(
+                        0, i, rustbox::Style::empty(),
+                        front, back, name.to_str().unwrap()
+                    );
+                }
+            }
+        }
+
+        self.rustbox.present();
     }
 }
 
@@ -145,20 +211,30 @@ fn main() {
 
     println!("loading...");
     let mut fsts = FSTree::from_root(&path).unwrap();
-    let mut ui_state = UIState::new();
     let rustbox = rustbox::RustBox::init(rustbox::InitOptions::default()).unwrap();
+    let mut ui_state = UI::new(&rustbox, &fsts);
 
     loop {
-        draw(&mut fsts, &rustbox);
+        ui_state.draw();
 
         match rustbox.poll_event(false) {
             Ok(rustbox::Event::KeyEvent(rustbox::keyboard::Key::Char('q'))) => break,
+
+            Ok(rustbox::Event::KeyEvent(rustbox::keyboard::Key::Char('k'))) => {
+                let mut o_cur = std::mem::replace(&mut ui_state.selected, None);
+                ui_state.selected = o_cur.map(|cur| if cur > 0 { cur - 1 } else { cur } );
+            },
+
+            Ok(rustbox::Event::KeyEvent(rustbox::keyboard::Key::Char('j'))) => {
+                let mut o_cur = std::mem::replace(&mut ui_state.selected, None);
+
+                ui_state.selected = o_cur.map(|cur|
+                    std::cmp::min(ui_state.listing.len() - 1, cur + 1)
+                );
+            },
+
             _ => (),
         }
     }
 }
 
-fn draw(fsts: &mut Contents, rustbox: &rustbox::RustBox) {
-    rustbox.clear();
-    rustbox.present();
-}
